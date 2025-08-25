ab668c4de4b4dd3f48110270604bd0a2c04626d4
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2025-08-25 22:54:35 +0200

    feat: Make `blue-run-command' understand command requirements
    Requirements include:
    - Running command in a comint mode compilation buffer if the command requires
    user interaction.
    - Running command in buffer where project was last configured if command
    requires configuration.

--- blue.el
+++ blue.el
@@ -33,52 +33,85 @@
   '((t :inherit completions-annotations))
   "Face used to highlight documentation strings.")
 
+(defvar blue-interactive-commands '("repl")
+  "List of strings of interactive BLUE commands.
+Interactive commands will run in comint mode compilation buffers.")
+
 (defvar blue--anotation-padding 8
   "Padding used for alignment of synopsis in completing read.")
+
+(defvar blue--last-configuration nil
+  "Path to last known configuration directory.")
 
 ;; Example output:
 ;; (((invoke . "build")
 ;;   (category . build)
-;;   (synopsys . "Build the project")
+;;   (synopsis . "Build the project")
 ;;   (help . "[INPUTS] ...\nCompile all blue modules or only INPUTS."))
 ;;  ...)
 (defun blue--get-commands ()
   ;; Run `blue' in `default-directory'.
-  (let ((commands-raw (shell-command-to-string "blue .elisp-serialize")))
+  (let ((commands-raw (shell-command-to-string "blue .elisp-serialize-commands")))
     (read commands-raw)))
 
 ;;;###autoload
-(defun blue-run-command (command)
+(defun blue-run-command (command &optional commands comint-flip)
   "Run a BLUE command."
   (interactive
    (let* ((commands (blue--get-commands))
-          (invocations (mapcar (lambda (c) (cdr (assq 'invoke c))) commands))
+          (invocations (mapcar (lambda (cmd)
+                                 (alist-get 'invoke cmd))
+                               commands))
           (width (apply #'max (mapcar #'string-width invocations)))
           (completion-extra-properties
            (list
             :annotation-function
             (lambda (cand)
-              (let* ((entry (seq-find (lambda (c)
-                                        (string= cand (cdr (assq 'invoke c))))
+              (let* ((entry (seq-find (lambda (cmd)
+                                        (string= cand
+                                                 (alist-get 'invoke cmd)))
                                       commands))
-                     (syn (cdr (assq 'synopsys entry))))
-                (when syn
+                     (synopsis (alist-get 'synopsis entry)))
+                (when synopsis
                   (concat (make-string (+ blue--anotation-padding
                                           (- width (string-width cand)))
                                        ?\s)
-                          (propertize syn 'face 'blue-documentation)))))
+                          (propertize synopsis 'face 'blue-documentation)))))
             :group-function
             (lambda (cand transform)
               (if transform
                   cand
-                (let* ((entry (seq-find (lambda (invoke)
+                (let* ((entry (seq-find (lambda (cmd)
                                           (string= cand
-                                                   (alist-get 'invoke invoke)))
+                                                   (alist-get 'invoke cmd)))
                                         commands))
                        (category (alist-get 'category entry)))
                   (symbol-name category)))))))
-     (list (completing-read "Command: " invocations))))
-  (message (concat "Running " command "...")))
+     (list (completing-read "Command: " invocations)
+           commands
+           (consp current-prefix-arg))))
+  ;; `xor' allows to use `comint-flip' to invert the mode for the compilation,
+  ;; if `command' is part of `blue-interactive-commands', calling
+  ;; `blue-run-command' with universal prefix argument (commint-flip = t) will
+  ;; disable comint mode for the compilation buffers while enabling it for all
+  ;; other commands.
+  (let* ((inter (if (xor (member command blue-interactive-commands)
+                         comint-flip)
+                    t nil))
+         (configuration (string= command "configure"))
+         (entry (seq-find (lambda (cmd)
+                            (string= command
+                                     (alist-get 'invoke cmd)))
+                          commands))
+         (requires-configuration (alist-get 'requires-configuration? entry)))
+    (when configuration
+      (setq blue--last-configuration default-directory))
+    ;; If the command requires a configuration cache, try to run it in the last
+    ;; configured directory.
+    (let ((default-directory (if requires-configuration
+                                 blue--last-configuration
+                               default-directory)))
+      (compile (concat "blue " command) inter))))
 
 (provide 'blue)
 ;;; blue.el ends here.
