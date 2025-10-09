67b058ca787bb173e0ae8becdf217e611759d36d
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2025-10-09 09:44:43 +0200

    chore: Bump package version to 0.0.8

--- blue.el
+++ blue.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2025 Sergio Pastor Pérez
 ;;
 ;; Author: Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
-;; Version: 0.0.7
+;; Version: 0.0.8
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: blue, tools
 ;; URL: https://codeberg.org/lapislazuli/blue.el
