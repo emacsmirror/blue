b54cd5a53cc3eb0ea295266538ff6bcf569191bb
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2026-01-01 06:59:26 +0100

    refactor: Remove some code duplication

--- blue-complete.el
+++ blue-complete.el
@@ -279,6 +279,22 @@
        :company-kind (lambda (_) 'property)
        :exclusive no)))
 
+(defun blue-completion--complete-autocompletable (autocompletable)
+  "Helper to produce completion table for autocompletable object respecting
+bounds."
+  (let* ((autocomplete (alist-get 'autocomplete autocompletable))
+         (type (alist-get 'type autocomplete))
+         (table (alist-get 'values autocomplete)))
+    (cond
+     ((string-equal type "directory")
+      (blue--complete-directory))
+     ((string-equal type "file")
+      (blue--complete-file))
+     ((string-equal type "system-name")
+      (blue--complete-system-name))
+     ((string-equal type "set")
+      (blue--complete-set table)))))
+
 
 ;;; Interfaces.
 
@@ -297,72 +313,18 @@
            (last-cmd (and last-cmd+args
                           (car (string-split last-cmd+args))))
            (cmd (and last-cmd (blue--get-command last-cmd commands)))
-           (options (alist-get 'options cmd))
-           (affixation-function
-            (lambda (candidates)
-              (mapcar
-               (lambda (candidate)
-                 (if-let* ((long-label (string-trim candidate "--" "="))
-                           (option (blue--get-option-from-label long-label options))
-                           (arguments (alist-get 'arguments option))
-                           (arg-name (alist-get 'name arguments)))
-                     (list candidate "" (propertize arg-name 'face 'blue-documentation))
-                   (list candidate "" "")))
-               candidates)))
-           (kind-function
-            (lambda (candidate)
-              (cond
-               ((string-prefix-p "--" candidate) 'property)
-               ((member candidate commands) 'command)
-               (t 'event))))
-           (options-doc-buffer-function
-            (lambda (candidate)
-              (when-let* ((long-label (string-trim candidate "--" "="))
-                          (option (blue--get-option-from-label long-label options))
-                          (doc (alist-get 'doc option))
-                          (arguments (alist-get 'arguments option))
-                          (arg-name (alist-get 'name arguments)))
-                (with-current-buffer (get-buffer-create "*blue-capf-doc*")
-                  (erase-buffer)
-                  (insert doc)
-                  (font-lock-add-keywords
-                   nil
-                   `((,arg-name . 'blue-documentation)))
-                  (font-lock-mode 1)
-                  (font-lock-ensure)
-                  (current-buffer)))))
-           (argument-completion-properties
-            (list
-             :affixation-function affixation-function
-             :company-doc-buffer options-doc-buffer-function
-             :company-kind kind-function
-             :exclusive 'no))
-           ;; Helper to produce completion table for autocompletable object
-           ;; respecting bounds.
-           (blue-completion--complete-autocompletable
-            (lambda (autocompletable bounds)
-              (let* ((autocomplete (alist-get 'autocomplete autocompletable))
-                     (type (alist-get 'type autocomplete))
-                     (table (alist-get 'values autocomplete)))
-                (cond
-                 ((string-equal type "directory")
-                  (blue--complete-directory))
-                 ((string-equal type "file")
-                  (blue--complete-file))
-                 ((string-equal type "system-name")
-                  (blue--complete-system-name))
-                 ((string-equal type "set")
-                  (blue--complete-set table)))))))
+           (options (alist-get 'options cmd)))
       (cond
        ;; UI option value completion.
-       ((and (looking-back
+       ((and (not cmd)
+             (looking-back
               (regexp-opt (ensure-list blue-complete--option-value-prefix) t)
               (pos-bol))
              (match-end 1))
         (let* ((thing (thing-at-point 'symbol))
                (long-label (string-trim thing "--" "="))
                (option (blue--get-option-from-label long-label ui-options)))
-          (funcall blue-completion--complete-autocompletable option bounds-at-pt)))
+          (blue-completion--complete-autocompletable option)))
        ;; Command option value completion.
        ((and (looking-back
               (regexp-opt (ensure-list blue-complete--option-value-prefix) t)
@@ -371,7 +333,7 @@
         (let* ((thing (thing-at-point 'symbol))
                (long-label (string-trim thing "--" "="))
                (option (blue--get-option-from-label long-label options)))
-          (funcall blue-completion--complete-autocompletable option bounds-at-pt)))
+          (blue-completion--complete-autocompletable option)))
         ;; UI option completion.
        ((and (not cmd)
              (looking-back "\\(^\\|\s\\|\t\\)-+[^\s]*" (pos-bol))
@@ -385,7 +347,7 @@
        ;; Command argument completion.
        (cmd
         (let ((blue-complete--option-value-prefix " ")) ; Arguments are space separated.
-          (funcall blue-completion--complete-autocompletable cmd bounds-at-pt)))
+          (blue-completion--complete-autocompletable cmd)))
        ;; Command invocation completion.
        ((not cmd)
         `( ,(car bounds-at-pt) ,(cdr bounds-at-pt)
