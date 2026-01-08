eb959df112eff84c2249279336318e84e1f61331
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2026-01-08 15:37:31 +0100

    style(transient): Indentation

--- blue-transient.el
+++ blue-transient.el
@@ -623,26 +623,27 @@ to be specially handled."
 
 ;; KLUDGE: find a way to make this assign string keys instead of characters.
 (defun assign-prefix (words)
-  (let* ((assignments (seq-reduce
-                       (lambda (acc word)
-                         (let* ((used (mapcar #'cadr acc))
-                                (key (apply #'string
-                                            (seq-reduce ; Find unused prefix.
-                                             (lambda (acc ch)
-                                               (let* ((prev (apply #'string acc))
-                                                      (prefix (concat prev (char-to-string ch))))
-                                                 (string-to-list
-                                                  (if (and (not (string-empty-p prev))
-                                                           (not (member prev used)))
-                                                      prev
-                                                    prefix))))
-                                             (string-to-list word)
-                                             nil))))
-                           (unless key
-                             (error "No unused character available in %s" word))
-                           (append acc `((,word ,key)))))
-                       (sort words)
-                       nil))
+  (let* ((assignments
+          (seq-reduce
+           (lambda (acc word)
+             (let* ((used (mapcar #'cadr acc))
+                    (key (apply #'string
+                                (seq-reduce ; Find unused prefix.
+                                 (lambda (acc ch)
+                                   (let* ((prev (apply #'string acc))
+                                          (prefix (concat prev (char-to-string ch))))
+                                     (string-to-list
+                                      (if (and (not (string-empty-p prev))
+                                               (not (member prev used)))
+                                          prev
+                                        prefix))))
+                                 (string-to-list word)
+                                 nil))))
+               (unless key
+                 (error "No unused character available in %s" word))
+               (append acc `((,word ,key)))))
+           (sort words)
+           nil))
          (prefixes (mapcar #'cadr assignments))
          (simple-prefixes (simplify-prefixes prefixes))
          (simple-assignments (seq-mapn (lambda (assignment simple-prefix)
