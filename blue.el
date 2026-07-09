883f35f210c4eb092ebd737cdc93db8d6bb2c647
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2026-07-09 13:42:02 +0200

    feat: Enable color in logs

--- blue.el
+++ blue.el
@@ -337,14 +337,15 @@ the build directory `blue--build-dir' exists before adding it."
 
 ;;; Deserialization.
 
-;; TODO: force colors so the log files are more readable.
 (defun blue--execute (options commands)
   "Execute BLUE serialization COMMANDS with OPTIONS and return parsed output.
 
 If RAW is non nil, the serialized string will not be evaluated."
   (let* ((default-directory (or (blue--get-build-dir) default-directory))
-         (process-environment (append '("GUILE_AUTO_COMPILE=0")
-                                      process-environment))
+         (process-environment
+          (append '("GUILE_AUTO_COMPILE=0"
+                    "BLUE_PREFERENCES=blue.ui:color-policy=always")
+                  process-environment))
          (args (append (or options '()) commands))
          (command-string (string-join (cons blue-binary args) " "))
          exit-code
