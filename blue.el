822a965f17f1860661b788a9caee5f1996ab0c1d
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2026-09-06 16:44:16 +0200

    refactor: Rely on `ansi-osc-handlers' for prettifying links

--- blue.el
+++ blue.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2025 Sergio Pastor Pérez
 ;;
 ;; Author: Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
-;; Version: 0.0.37
+;; Version: 0.0.38
 ;; Package-Requires: ((emacs "30.1") (magit-section "4.3.8"))
 ;; Keywords: blue, tools
 ;; URL: https://codeberg.org/lapislazuli/blue.el
@@ -563,61 +563,29 @@ COMINT-P selects `comint-mode' for compilation buffer."
       (blue--visit-location path line col)))
    (t (browse-url url))))
 
-(defun blue-ansi-buttonize-hyperlinks (beg end)
-  "Convert ANSI OSC 8 hyperlink sequences into clickable buttons in the region."
-  (save-excursion
-    (goto-char beg)
-    ;; The marker will update its location as text is inserted or deleted.
-    (let ((limit (copy-marker end t)))
-      (while (re-search-forward
-              (rx "\033]8;;"
-                  (group (* (not (any "\033" "\007")))) ; URL capture group.
-                  (or "\033\\" "\007"))                 ; Terminator.
-              limit t)
-        (let* ((url (match-string-no-properties 1))
-               (open-start (match-beginning 0))
-               (open-end (match-end 0))
-               (text-start (copy-marker open-start))
-               (text-end nil))
-
-          ;; Remove opening OSC 8 sequence.
-          (delete-region open-start open-end)
-
-          ;; Find closing OSC 8 sequence.
-          (goto-char text-start)
-          (when (re-search-forward
-                 (rx "\033]8;;" (or "\033\\" "\007"))
-                 limit t)
-            (setq text-end (copy-marker (match-beginning 0)))
-
-            ;; Remove closing OSC 8 sequence.
-            (delete-region (match-beginning 0) (match-end 0))
-
-            ;; Add button to the visible text.
-            (when (> (marker-position text-end)
-                     (marker-position text-start))
-              (make-text-button
-               (marker-position text-start)
-               (marker-position text-end)
-               'action (lambda (_)
-                         (blue-open-hyperlink url))
-               'follow-link t
-               'help-echo (format "Click to open %s" url)))))))))
-
-(defun blue-hyperlinks-compilation-filter ()
-  "Translate OSC hyperlink escape sequences button text properties."
-  (let ((inhibit-read-only t))
-    (blue-ansi-buttonize-hyperlinks compilation-filter-start (point))))
+(defun blue-ansi-osc-hyperlink-handler (_ text)
+  "Like `ansi-osc-hyperlink-handler' but handle file line and column."
+  (when ansi-osc-hyperlink--state
+    (let ((start (car ansi-osc-hyperlink--state))
+          (url (cdr ansi-osc-hyperlink--state)))
+      (make-text-button start (point)
+                        'action (lambda (_)
+                                  (blue-open-hyperlink url))
+                        'help-echo (format "Click to open %s" url))))
+  (setq ansi-osc-hyperlink--state
+        (and (string-match ";\\(.+\\)" text)
+             (cons (point-marker) (match-string-no-properties 1 text)))))
 
 (defun blue-prettify-compilation-filter ()
   "Combination of filters to prettify output in compilation buffers."
-  ;; HACK: apply first our own OSC filter since the built-in one,
-  ;; `ansi-osc-compilation-filter', does not visit lines properly. This
-  ;; ensures our text properties are applied before the OSC filter does
-  ;; anything.
-  (blue-hyperlinks-compilation-filter)
+  ;; HACK: apply first our own OSC link handler since the built-in one,
+  ;; `ansi-osc-hyperlink-handler', does not visit lines properly.
   (ansi-color-compilation-filter)
-  (ansi-osc-compilation-filter))
+  (let ((ansi-osc-handlers (cons
+                            '("8" . blue-ansi-osc-hyperlink-handler)
+                            (alist-get "8" ansi-osc-handlers nil t)))
+        (ansi-osc-for-compilation-buffer t))
+    (ansi-osc-compilation-filter)))
 
 (defun blue--apply-filter-in-region (beg end filter)
   "Helper to apply FILTER to string STR."
@@ -639,10 +607,11 @@ COMINT-P selects `comint-mode' for compilation buffer."
 
 (defun blue-prettify-comint-filter (_)
   "Combination of filters to prettify output in comint buffers."
-  (dolist (filter '(blue-ansi-buttonize-hyperlinks
-                    ansi-color-apply-on-region
-                    ansi-osc-apply-on-region))
-    (blue--run-comint-filter filter)))
+  (blue--run-comint-filter #'ansi-color-apply-on-region)
+  (let ((ansi-osc-handlers (cons
+                            '("8" . blue-ansi-osc-hyperlink-handler)
+                            (alist-get "8" ansi-osc-handlers nil t))))
+    (blue--run-comint-filter #'ansi-osc-apply-on-region)))
 
 ;;;###autoload
 (define-minor-mode blue-prettify-compilation-mode
